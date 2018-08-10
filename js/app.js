f957c7bf80e0f49efd296c4a34188ef2c9b0745a
Paulina Dzwonek <paulinadzwonek92@gmail.com>
2018-08-10 08:38:11 +0200

    Animated mobile menu

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 var humburger = document.getElementById('humburger');
-console.log(humburger);
-
 
 humburger.addEventListener('click', function() {
+    //humburger animation//
+
+    var humburgerItem = document.getElementsByClassName('mobile__item');
+
+    for(i=0; i<humburgerItem.length; i++){
+        humburgerItem[i].classList.toggle('mobile--change');
+    }
+
+    //menu animation//
+
     var visibilityOfMenu = document.getElementById('menu');
+
     visibilityOfMenu.classList.toggle('nav--unvisible');
+
 });
