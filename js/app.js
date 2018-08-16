3ffb8f7b8edd7ef5514ccef7eaf62b068f575ab1
Paulina Dzwonek <paulinadzwonek92@gmail.com>
2018-08-16 18:28:43 +0200

    Finished gallery slider on mobile

--- js/app.js
+++ js/app.js
@@ -16,3 +16,35 @@ humburger.addEventListener('click', function() {
     visibilityOfMenu.classList.toggle('nav--unvisible');
 
 });
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    //gallery slider//
+
+    var leftBtn = document.getElementById('left-btn');
+    var rightBtn = document.getElementById('right-btn');
+    var listElements = document.getElementsByClassName('gallery__photo');
+
+    console.log(listElements);
+
+    var indexOfPicture = 0;
+    listElements[indexOfPicture].classList.add('gallery__photo--visible');
+
+    leftBtn.addEventListener('click', function(){
+        listElements[indexOfPicture].classList.remove('gallery__photo--visible');
+        indexOfPicture --;
+        if(indexOfPicture < 0) {
+            indexOfPicture = listElements.length -1;
+        }
+        listElements[indexOfPicture].classList.add('gallery__photo--visible');
+    });
+    rightBtn.addEventListener('click', function(){
+        listElements[indexOfPicture].classList.remove('gallery__photo--visible');
+        indexOfPicture ++;
+        if(indexOfPicture > listElements.length -1) {
+            indexOfPicture = 0;
+        }
+        listElements[indexOfPicture].classList.add('gallery__photo--visible');
+    });
+
+});
