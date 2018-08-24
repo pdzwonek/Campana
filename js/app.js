5c48d2076c8a386800bec3a7e3614cab502d4721
Paulina Dzwonek <paulinadzwonek92@gmail.com>
2018-08-24 20:24:36 +0200

    Changes in gallery section

--- js/app.js
+++ js/app.js
@@ -19,32 +19,63 @@ humburger.addEventListener('click', function() {
 
 document.addEventListener('DOMContentLoaded', function(){
 
+    var width = window.innerWidth;
+
     //gallery slider//
 
     var leftBtn = document.getElementById('left-btn');
     var rightBtn = document.getElementById('right-btn');
-    var listElements = document.getElementsByClassName('gallery__photo');
+    var galleryElements = document.getElementsByClassName('gallery__photo');
 
-    console.log(listElements);
+    var galleryBox = document.getElementById('gallery-content');
+    var galleryPhoto = document.getElementById('gallery-photo');
+    var arrows = document.getElementById('gallery-arrows');
 
-    var indexOfPicture = 0;
-    listElements[indexOfPicture].classList.add('gallery__photo--visible');
+    galleryBox.scrollLeft = ((galleryBox.offsetWidth - galleryPhoto.offsetWidth) /2);
 
-    leftBtn.addEventListener('click', function(){
-        listElements[indexOfPicture].classList.remove('gallery__photo--visible');
-        indexOfPicture --;
-        if(indexOfPicture < 0) {
-            indexOfPicture = listElements.length -1;
-        }
-        listElements[indexOfPicture].classList.add('gallery__photo--visible');
-    });
-    rightBtn.addEventListener('click', function(){
-        listElements[indexOfPicture].classList.remove('gallery__photo--visible');
-        indexOfPicture ++;
-        if(indexOfPicture > listElements.length -1) {
-            indexOfPicture = 0;
-        }
-        listElements[indexOfPicture].classList.add('gallery__photo--visible');
-    });
+
+    // if(width <= 767) {
+    //
+    //     var indexOfPicture = 0;
+    //
+    //     leftBtn.addEventListener('click', function(){
+    //         listElements[indexOfPicture].classList.remove('gallery__photo--visible');
+    //         indexOfPicture --;
+    //         if(indexOfPicture < 0) {
+    //             indexOfPicture = listElements.length -1;
+    //         }
+    //         listElements[indexOfPicture].classList.add('gallery__photo--visible');
+    //     });
+    //     rightBtn.addEventListener('click', function(){
+    //         listElements[indexOfPicture].classList.remove('gallery__photo--visible');
+    //         indexOfPicture ++;
+    //         if(indexOfPicture > listElements.length -1) {
+    //             indexOfPicture = 0;
+    //         }
+    //         listElements[indexOfPicture].classList.add('gallery__photo--visible');
+    //     });
+    // }
+
+    // if(width > 767) {
+    //
+    //     var indexOfPhoto = 1;
+    //     listElements[indexOfPhoto].classList.add('gallery__photo--visible');
+    //     leftBtn.addEventListener('click', function(){
+    //         listElements[indexOfPicture].classList.remove('gallery__photo--visible');
+    //         indexOfPicture --;
+    //         if(indexOfPicture < 0) {
+    //             indexOfPicture = listElements.length -1;
+    //         }
+    //         listElements[indexOfPicture].classList.add('gallery__photo--visible');
+    //     });
+    //     rightBtn.addEventListener('click', function(){
+    //         listElements[indexOfPicture].classList.remove('gallery__photo--visible');
+    //         indexOfPicture ++;
+    //         if(indexOfPicture > listElements.length -1) {
+    //             indexOfPicture = 0;
+    //         }
+    //         listElements[indexOfPicture].classList.add('gallery__photo--visible');
+    //     });
+    // }
 
 });
